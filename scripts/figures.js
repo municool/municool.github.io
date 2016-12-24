0e2835cdaa390f9e5f7cea64a138b00014e48a60
municool <herren.louis@gmx.ch>
2016-12-24 23:14:48 +0100

    created little changes

--- scripts/figures.js
+++ scripts/figures.js
@@ -2,21 +2,34 @@
  * Created by nvmuni on 23.12.16.
  */
 
-function bauer(pos, faction){
+function bauer(pos, faction) {
     this.pos = pos;
     this.faction = faction;
+    var isActive = false;
+    this.strokeColor = color(255);
 
-    this.drawFigure = function () {
-        if(faction === "black"){
-            fill(255);
-        }
-        else{
+    this.display = function () {
+        stroke(this.strokeColor);
+        if (faction === "black") {
             fill(0);
         }
-        ellipse()
+        else {
+            fill(255);
+        }
+        ellipse(mouseX, mouseY, 20, 20);
     }
 
     this.makeMove = function () {
         //move shit
+    }
+
+    this.changeActivity = function () {
+        if (isActive) {
+            isActive = false;
+        }
+        else {
+            this.strokeColor = color(252,82,82,255);
+            isActive = true;
+        }
     }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
  * Created by nvmuni on 20.12.16.
  */
 
+var figures = [];
 
 function setup() {
     createCanvas(600, 600);
@@ -13,5 +14,14 @@ function draw() {
     var playGround = new PlayGround(600, 8);
     playGround.drawPlayGround();
 
+    figures.push(new bauer(50,"black"));
+    for(var i = 0; i< figures.length; i++){
+        figures[i].display();
+    }
+}
 
+function mouseClicked() {
+    for(var i = 0; i< figures.length; i++){
+        figures[i].changeActivity();
+    }
 }
